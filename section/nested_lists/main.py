9375ed80fa120116bc184e04d53eb1304e1036fb
shubhammarcus <>
2026-05-21 08:45:26 +0000

    Nested lists

---
+++ section/nested_lists/main.py
@@ -0,0 +1,10 @@
+# Put in the lists `[city name, country, the estimated cost]`
+first_city = ["Paris", "France", 2000]
+second_city = ["Tokyo", "Japan", 3500]
+third_city = ["Berlin", "Germany", 3400]
+
+# Put variables in the list 
+travel_wishlist = [first_city, second_city, third_city]
+
+# Testing
+print('travel_wishlist:', travel_wishlist)
